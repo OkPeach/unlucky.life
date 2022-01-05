cf7a00024b06b0f72de7b903190ea35610b09004
OkPeach <lucienismeme@gmail.com>
2022-01-05 14:02:21 +0100

    progress bar:)

---
+++ javascript/bar.js
@@ -0,0 +1,18 @@
+const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+const progressbar = () => {
+
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+    const scrolled = (winScroll / height) * 100;
+    
+    document.getElementById("progress-bar").style.width = scrolled + "%";
+
+    //console.log(scrolled + '%')
+};
+
+window.onscroll = function() {
+    progressbar();
+};
