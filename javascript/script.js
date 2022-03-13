96521e1a92a5c800d1f3c40782f5b102eda875bd
OkPeach <lucienismeme@gmail.com>
2022-03-13 19:22:10 +0100

    Made animations play after loading so they will be probably perfectly synced with loading screen

--- javascript/script.js
+++ javascript/script.js
@@ -268,10 +268,15 @@ function fadeOutEffect() {
 
 $(window).on('load', function () {
   setTimeout(function () {
+    //locate "target" aka the preload screen and slowly transition to 0 opacity and after transition deletes the whole element
     const target = document.getElementById("loading");
     target.style.opacity = '0'
     target.addEventListener('transitionend', () => target.remove());  
+
+    //adds animation after load
+    $("#logo").addClass("logoAnim");
+    $("#infoCard").addClass("cardAnim");
   }, 1250);
 
-  console.log('Loaded!')
+  console.log('Loaded, welcome!')
 }) 
