8f31cf80447b503b2c5c95f51b1a8749c2ea0e4d
OkPeach <lucienismeme@gmail.com>
2022-05-23 16:39:41 +0200

    New mobile site

---
+++ javascript/m.js
@@ -0,0 +1,21 @@
+//credit: DenverCoder1 edited by me
+function getAge(dateString) {
+    var ageInMilliseconds = new Date() - new Date(dateString);
+    var years = Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
+    return ' ' + years + ' years old'
+  }
+  //console.log(getAge('2002-06-24'));
+
+  $(window).on('load', function () {
+  document.getElementById("age").innerHTML = getAge('2002-06-24');
+  });
+
+function onload(){
+    document.getElementById("line").classList.add("lineanimation");
+    document.getElementById("line2").classList.add("lineanimation");
+
+    document.getElementById("about").classList.add("aboutanimation");
+    document.getElementById("hobbies").classList.add("hobbiesanimation");
+
+    document.getElementById("name").classList.add("nameanimation");
+}
