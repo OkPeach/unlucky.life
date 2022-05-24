bb79918a551c7d018acf5dc8f21b90533fec1bab
OkPeach <lucienismeme@gmail.com>
2022-05-24 14:29:35 +0200

    Added projects to the mobile site

--- javascript/m.js
+++ javascript/m.js
@@ -22,3 +22,20 @@ function onload(){
     document.getElementById("header").classList.add("headeranim");
     document.getElementById("menuToggle").classList.add("menutoggleanimation");
 }
+
+
+//animate when in viewport (https://stackoverflow.com/questions/27462306/css3-animate-elements-if-visible-in-viewport-page-scroll)
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+  Obs.observe(EL, obsOptions);
+});
