5c9e2e5bc99ac046e19d4f34f6347463dd05b5a3
OkPeach <lucienismeme@gmail.com>
2022-06-11 18:43:03 +0200

    Added time and date to taskbar and made a closing button for it

--- javascript/script.js
+++ javascript/script.js
@@ -646,5 +646,48 @@ $(document).keyup(function(event) {
     console.log('Windows bar closed')
   }
   }
+}});
+
+//check time
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
-});
+
+//get current time and set it in element with class time
+function getTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(getTime, 10);
+}
+
+function getDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = '0' + dd;
+  }
+
+  if (mm < 10) {
+    mm = '0' + mm;
+  }
+
+  today = mm + '.' + dd + '.' + yyyy;
+  document.getElementById('date').innerHTML = today;
+}
+
+function hideNotes() {
+  devmenu.addClass('windows-close-animation');
+  open = false;
+  console.log('Windows bar closed')
+}
